36dd86c28532996928e9ad63958714cefce69fc6
Kyle Saunders <kyle.saunders1310@gmail.com>
2022-01-15 23:30:09 +0200

    Updated project model.

---
+++ models/project-resource.model.ts
@@ -0,0 +1,14 @@
+export class ProjectResource {
+  type: string | undefined
+  name: string | undefined
+  url: string | undefined
+
+  public static fromJson(jsonObj: Record<string, unknown>): ProjectResource {
+      const projectResource = new ProjectResource()
+      projectResource.type = jsonObj.type as string
+      projectResource.name = jsonObj.name as string
+      projectResource.url = jsonObj.url as string
+
+      return projectResource
+  }
+}
